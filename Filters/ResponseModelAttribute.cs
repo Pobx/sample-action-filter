4e54dd02b780a34cdb2af3aa4d5a535428771c09
Tia <tiakun@users.noreply.github.com>
2020-09-22 14:27:44 +0700

    refactor: Replace ObjectResult.Value instead

--- Filters/ResponseModelAttribute.cs
+++ Filters/ResponseModelAttribute.cs
@@ -11,12 +11,7 @@ namespace sample_action_filter.Filters {
       var result = context.Result as ObjectResult;
       var response = new MyResponse<object> ();
       response.Entities = result?.Value;
-
-      if (context.ModelState.IsValid && result is OkObjectResult) {
-        context.Result = new OkObjectResult (response);
-      } else if (context.ModelState.IsValid && result is CreatedResult) {
-        context.Result = new CreatedResult ("", response);
-      }
+      result.Value = response;
     }
 
   }
